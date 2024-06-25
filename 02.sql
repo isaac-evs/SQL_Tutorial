666379bb1236dce6d597b62df266bfb843be7a92
isaac-evs <122956526+isaac-evs@users.noreply.github.com>
2024-06-25 17:50:17 -0600

    Update 02.sql

--- 02.sql
+++ 02.sql
@@ -7,3 +7,40 @@ CREATE TABLE employees (
     hourly_pay DECIMAL(5, 2),
     hire_date DATE 
 );
+
+-- Select Table 
+
+SELECT * FROM employees; 
+
+-- Rename Table
+
+RENAME TABLE workers TO employees;
+
+-- Add another column
+
+ALTER TABLE employees 
+ADD phone_number VARCHAR(15);
+
+-- Rename column
+
+ALTER TABLE employees 
+RENAME COLUMN phone_number TO email
+
+-- Modify column propierties
+
+ALTER TABLE employees
+MODIFY COLUMN email VARCHAR(100);
+
+-- Move column
+
+ALTER TABLE employees
+MODIFY email VARCHAR(100)
+AFTER last_name;
+
+ALTER TABLE employees
+MODIFY email VARCHAR(100)
+FIRST;
+
+-- Delete column
+ALTER TABLE employees
+DROP COLUMN email;
