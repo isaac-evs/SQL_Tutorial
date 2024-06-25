18647a1f442edc93bfc80d8a8fef3fd4a6279ab7
isaac-evs <122956526+isaac-evs@users.noreply.github.com>
2024-06-25 17:59:20 -0600

    Create 03.sql

---
+++ 03.sql
@@ -0,0 +1,12 @@
+-- Insert Rows
+
+INSERT INTO employees
+VALUES (1,"Eugene","Krabs",25.50, "2023-01-02");
+
+-- Insert Multiple Rows
+
+INSERT INTO employees
+VALUES  (2,"Squidward","Tentacles",15.00, "2023-01-03"),
+		    (3, "Spongebob", "Squarepants", 12.50, "2023-01-04"),
+        (4, "Patrick", "Star", 12.50, "2023-01-05"),
+        (5, "Sandy", "Cheeks", 17.25, "2023-01-06");
